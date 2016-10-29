594eadcd1beb163026b3044a2ece8508f0730177
shadoworzil <shadow.orzil6@gmail.com>
2016-10-29 09:56:33 -0400

    final

--- ejemplo4.js
+++ ejemplo4.js
@@ -1,6 +1,12 @@
-console.log("Suma de Dos numeros");
-console.log (3+7);
-console.log("Resta de los numeros 13-7:");
+console.log("Suma de Dos numeros3+7 es: ");
+console.log(3+7);
+console.log("Resta de los numeros 13-7 es : ");
 console.log(13-7);
-console.log("expresion con parentesis (8*2-4)/3:");
-console.log((8*2-4)/3);
+console.log("expresion con parentesis (8*2-4)/3 es : ");
+console.log(8*2-4)/3;
+console.log("");
+console.log("=>"+(13+7));
+console.log("=>"+("13"+"7"));
+console.log("=>"+("13"+7));
+console.log("=>"+(+"13"+7));
+console.log("=>"+(+"40"+"50"));
--- ejemplo6.js
+++ ejemplo6.js
@@ -1,6 +1,3 @@
-console.log("Suma de Dos numeros"+(3+7);
-console.log (3+7);
-console.log("Resta de los numeros 13-7:");
-console.log(13-7);
-console.log("expresion con parentesis (8*2-4)/3:");
-console.log((8*2-4)/3);
+console.log("Suma de Dos numeros3+7 es: "+(3+7));
+console.log("Resta de los numeros 13-7 es : "+(13-7));
+console.log("expresion con parentesis (8*2-4)/3 es : "+(8*2-4)/3);
