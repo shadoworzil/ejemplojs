282d3d18e8c6510ce667936cc063f4630fcd9eb3
shadoworzil <shadow.orzil6@gmail.com>
2016-10-29 09:22:11 -0400

    lol

--- ejemplo4.js
+++ ejemplo4.js
@@ -1,5 +1,5 @@
-console.log("Suma de Dos numeros"+(3+7);
-console.log );
+console.log("Suma de Dos numeros");
+console.log (3+7);
 console.log("Resta de los numeros 13-7:");
 console.log(13-7);
 console.log("expresion con parentesis (8*2-4)/3:");
